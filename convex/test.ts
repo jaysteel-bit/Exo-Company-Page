54d81924d7f23f700d19f789295b88f626a1c2b8
Jay Steel <officialjaysteel@gmail.com>
2026-02-17 21:03:19 -0500

    chore: remove unwanted test files

--- convex/test.ts
+++
@@ -1,8 +0,0 @@
-import { query } from "./_generated/server";
-
-export const hello = query({
-  args: {},
-  handler: async (ctx) => {
-    return "Connection Successful: Hello from your Convex Database!";
-  },
-});
